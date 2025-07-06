e2edbeebbac524608fea54b08534fa8a246b456b
akongstad <kongstad@outlook.com>
2025-07-06 12:05:30 +0200

    feat: add terminal folder with starship, fish, and ghostty

---
+++ terminal/fish/config.fish
@@ -0,0 +1,10 @@
+/opt/homebrew/bin/brew shellenv | source
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias idea='open -b com.jetbrains.intellij'
+
+starship init fish | source
+# EOF
